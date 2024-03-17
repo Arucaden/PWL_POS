69b5ae1fb284d0ae3da205252b03a4225311f9a4
Arucaden <110232305+Arucaden@users.noreply.github.com>
2024-03-18 00:26:30 +0700

    P-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index() {
         // $data = [
         //     'username' => 'customer-1',
         //     'nama' => 'Pelanggan',
@@ -25,15 +25,23 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view('user', ['data'=>$user]);
 
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
-        $user = UserModel::all();
-        return view('user', ['data'=>$user]);
+        // $user = UserModel::all();
+        // return view('user', ['data'=>$user]);
+
+        // $user = UserModel::firstWhere('level_id', 1);
+        // return view('user', ['data' => $user]);
+        
+        $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
     }
 }
