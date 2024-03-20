dc75c1d93fa3378c868149923de5d84943e1e473
Arucaden <110232305+Arucaden@users.noreply.github.com>
2024-03-20 15:40:21 +0700

    JS 5 P-3

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,11 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+// Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
